0c96b1c272a62238e77c8790c6d2dc4113780516
Robert Reimann <robi.reimann@gmail.com>
2020-10-25 19:35:34 +0200

    task2

--- res/js/script.js
+++ res/js/script.js
@@ -1,3 +1,5 @@
+let posts = [];
+
 $(function() {
     loadProfile();
     $('.avatar').click(function() {
@@ -9,7 +11,21 @@ $(function() {
         }
     });
 
+    $('.like-button').click(function () {
+        $(this).css('background-color', 'pink');
+    });
 
+    loadPosts()
+        .then(function (response) {
+        for (let post of response) {
+            posts.push(new Post(post.id, post.author, post.createTime, post.text, post.media, post.likes))
+        }
+
+        displayPosts()
+
+    }).catch(function () {
+        alert('error loading posts')
+    });
 });
 
 
@@ -39,4 +55,54 @@ function loadProfile() {
             $('#user-email').text(profile.email);
             $('img.avatar').attr("src", response.avatar);
         })
+}
+
+function loadPosts() {
+    return $.get({
+        url: 'https://private-anon-2d159cc08d-wad20postit.apiary-mock.com/posts',
+        success: function(response) {
+            return response;
+        },
+        error: function () {
+            alert('error loading posts!')
+        }
+    });
+}
+
+
+function displayPosts() {
+    for (let post of posts) {
+
+        let postDiv = jQuery('<div/>', {
+            "class": 'post '
+        })
+
+        let postAuthor = jQuery('<div/>', {
+            "class": 'post-author '
+        })
+        postAuthor.append('<span/>'.class('post-author-info ').append('<small/>'.text(post.author)))
+        postAuthor.append('<small/>'.text(post.createTime))
+
+        let postImage = jQuery('<div/>', {
+            "class": "post-image "
+        })
+        postImage.append('<img src=post.media/>')
+
+        let postTitle = jQuery('<div/>', {
+            "class": "post-title "
+        })
+        postTitle.append('<h3/>'.text(post.text))
+
+        let postActions = jQuery('<div/>', {
+            "class": "post-actions "
+        })
+        postActions.append('<button/>'.name("like").type("button").class("like-button").value(post.likes))
+
+        postDiv.append(postAuthor)
+        postDiv.append(postImage)
+        postDiv.append(postTitle)
+        postDiv.append(postActions)
+
+        $('body.section').append(postDiv)
+    }
 }
